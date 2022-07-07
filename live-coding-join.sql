b7f7c66aa3996c9dfd00398745e02796ea5e0c61
costanzazanoletti <zanoletti.cos@gmail.com>
2022-07-07 12:58:21 +0200

    conclusione live coding

--- live-coding-join.sql
+++ live-coding-join.sql
@@ -45,13 +45,54 @@ INNER JOIN paganti pag
 ON pa.pagante_id = pag.id
 ORDER BY pag.name , pag.lastname ; 
 
+-- Selezionare tutte le prenotazioni ordinate per data di creazione, 
+-- mostrando i dati della stanza e della configurazione
+SELECT p.id , p.created_at , s.room_number , s.`floor` , c.title 
+FROM prenotazioni p 
+INNER JOIN stanze s 
+ON p.stanza_id = s.id 
+INNER JOIN configurazioni c 
+ON p.configurazione_id = c.id
+ORDER BY p.created_at; 
+
+-- quante prenotazioni sono state fatte per ogni stanza e mostrarle in ordine discendente
+SELECT s.room_number, COUNT(p.id) 
+FROM stanze s 
+LEFT OUTER JOIN prenotazioni p 
+ON s.id = p.stanza_id
+GROUP BY s.room_number 
+ORDER BY s.room_number; 
+
+-- quanto ha speso in media ogni pagante prendendo solo quelli che hanno un ospite associato
+SELECT p2.name , p2.lastname, AVG(p.price) as average_price
+FROM pagamenti p 
+INNER JOIN paganti p2
+ON p.pagante_id = p2.id
+WHERE p.status = 'accepted' AND p2.ospite_id IS NOT NULL
+GROUP BY p2.name , p2.lastname 
+ORDER BY average_price DESC; 
+
+
+-- DIVAGAZIONE SULLE VIEW
+-- una view è una select che viene salvata in una tabella virtuale
+CREATE VIEW stanze_con_piano AS
+SELECT `floor`, COUNT(*) as numero_stanze
+FROM stanze
+GROUP BY `floor`
+ORDER BY `floor` ASC;
+
+SELECT * FROM stanze_con_piano;
 
 
 
+INSERT INTO stanze
+(room_number, floor, beds, created_at, updated_at)
+VALUES(110, 1, 5, '2022-07-07 12:17:10', '2022-07-07 12:17:10');
 
 
-
-
-
+SELECT *
+FROM ospiti o 
+INNER JOIN paganti p 
+ON LOWER(o.name) = LOWER(p.name) AND LOWER(o.lastname)  = LOWER(p.lastname); 
 
 
